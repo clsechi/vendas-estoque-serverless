47ac2857598a906053e4396165c7dc9c58f15ec0
Carlos Sechi <carlos.sechi@frentecorretora.com.br>
2018-05-20 00:14:28 -0300

    First commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page padding class="docs-input">
+    <div style="width: 500px; max-width: 90vw;">
+      <q-input v-model="text" float-label="Float Label" placeholder="Gigi" />
+      <q-input v-model="text" float-label="Float Label" placeholder="Gigi" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
