cb27426a62cfa9caf5beedc345c64eb0bd325027
Carlos Sechi <carlos.sechi@frentecorretora.com.br>
2018-05-20 20:03:25 -0300

    some sytle changes

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -15,11 +15,19 @@
         >
           <q-icon name="menu" />
         </q-btn>
-
         <q-toolbar-title class="">
           Estoque do Sechi
         </q-toolbar-title>
       </q-toolbar>
+      <q-tabs>
+        <q-route-tab
+          slot="title"
+          icon="assignment"
+          to="/admin"
+          replace
+          label="Administração"
+        />
+      </q-tabs>
     </q-layout-header>
 
     <q-layout-drawer
@@ -35,22 +43,6 @@
         <q-item @click.native="$router.push('/admin')">
           <q-item-side icon="school" />
           <q-item-main label="Admin"/>
-        </q-item>
-        <q-item @click.native="openURL('https://github.com/quasarframework/')">
-          <q-item-side icon="code" />
-          <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
-        </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
-        </q-item>
-        <q-item @click.native="openURL('http://forum.quasar-framework.org')">
-          <q-item-side icon="record_voice_over" />
-          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-        </q-item>
-        <q-item @click.native="openURL('https://twitter.com/quasarframework')">
-          <q-item-side icon="rss feed" />
-          <q-item-main label="Twitter" sublabel="@quasarframework" />
         </q-item>
       </q-list>
     </q-layout-drawer>
@@ -68,7 +60,8 @@ export default {
   name: 'LayoutDefault',
   data () {
     return {
-      leftDrawerOpen: this.$q.platform.is.desktop
+      leftDrawerOpen: false
+      // this.$q.platform.is.desktop
     }
   },
   methods: {
